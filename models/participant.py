3953299259214f77944eceb94c961f409241c522
tiurmagrace <tiursihaloho210905@gmail.com>
2025-05-16 21:10:14 +0700

    Menghapus semua file dan folder lama

--- models/participant.py
+++
@@ -1,6 +0,0 @@
-from models.rsvp_status import RSVPStatus
-
-class Participant:
-    def __init__(self, name: str):
-        self.name = name
-        self.status = RSVPStatus.UNCONFIRMED
--- models/rsvp_status.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class RSVPStatus(Enum):
-    UNCONFIRMED = "Belum Dikonfirmasi"
-    ATTENDING = "Hadir"
-    NOT_ATTENDING = "Tidak Hadir"
